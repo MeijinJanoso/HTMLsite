fae6ee8fc35ef7067606d45ef1ccb1548c9028f5
MeijinJanoso <jpiechowicz@simonc.com.pl>
2014-10-23 00:53:44 +0200

    Strona simonc.com.pl
    Dynamiczna strona stwożona przy pomocy jQuery

---
+++ Simonc.com.pl - jQuery/index.php
@@ -0,0 +1,23 @@
+﻿<?php 
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+switch($lang)
+{
+   case 'pl':
+      include('./pl/index.html');       
+
+      break;
+   case 'en':
+      include('./en/index.html');
+
+   case 'de':
+      include('./de/index.html');
+
+	  
+      break;
+   default:
+      include('./en/index.html');   
+
+}
+
+?>
---
+++ Simonc.com.pl - jQuery/js/jqscr.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#ST").click(function(){
+		$("#ciallo").load("test.html")
+	});
+})
